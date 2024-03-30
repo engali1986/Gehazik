8adc740bd355481fe20e2a3c357f440d5a0c5eef
Ali Mohammed <104313196+engali1986@users.noreply.github.com>
2024-03-30 21:00:21 +0300

    first commit'
    '

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,28 @@
-import logo from './logo.svg';
-import Home from "./screens/Home.js"
-import './App.css';
-
+import logo from "./logo.svg";
+import Home from "./screens/Home.js";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
+    <div
+      className="App container"
+      style={{ border: "2px solid red", fontSize: "1rem" }}
+    >
+      <div className=" position-fixed w-100" style={{minHeight:'10vh', backgroundColor:'green',marginLeft:'inherit', marginRight:'inherit'}}>
+
+      </div>
       
+
+      
+       
+      <Home />
+      
+     
+      <div style={{minHeight:'200vh',border:'2px solid green'}}>
+
+      </div>
+
+     
     </div>
   );
 }
--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -2,39 +2,44 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      <div className="accordion accordion-flush" id="accordionFlushExample">
-  <div className="accordion-item">
-    <h2 className="accordion-header">
-      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-        Accordion Item #1
-      </button>
-    </h2>
-    <div id="flush-collapseOne" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-      <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> className. This is the first item's accordion body.</div>
-    </div>
-  </div>
-  <div className="accordion-item">
-    <h2 className="accordion-header">
-      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-        Accordion Item #2
-      </button>
-    </h2>
-    <div id="flush-collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-      <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> className. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
-    </div>
-  </div>
-  <div className="accordion-item">
-    <h2 className="accordion-header">
-      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-        Accordion Item #3
-      </button>
-    </h2>
-    <div id="flush-collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-      <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> className. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
-    </div>
-  </div>
-</div>
+    <div
+      className="d-none d-md-flex container row align-items-start"
+      style={{
+        minHeight: "5vh",
+        backgroundColor: "lightGray",
+        borderRadius: "5px",
+      }}
+    >
+      <div
+        className=" col-1 "
+        style={{
+          border: "2px solid black",
+          marginTop: "0.5%",
+          textAlign: "start",
+        }}
+      >
+        Gehazek
+      </div>
+      <div
+        className="  col-1 "
+        style={{
+          border: "2px solid black",
+          marginTop: "0.5%",
+          textAlign: "start",
+        }}
+      >
+        Profile
+      </div>
+      <div
+        className="  col-1"
+        style={{
+          border: "2px solid black",
+          marginTop: "0.5%",
+          textAlign: "start",
+        }}
+      >
+        Discounts
+      </div>
     </div>
   );
 };
