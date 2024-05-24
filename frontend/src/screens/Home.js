6d029a6c5b54486726e5fbe50511a1785ced0969
Ali Mohammed <104313196+engali1986@users.noreply.github.com>
2024-05-24 17:32:19 +0300

    SmallScreennavbar

--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -1,202 +1,42 @@
-import React, { useRef } from "react";
+import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const Home = (params) => {
   return (
     <Container>
-      <div className=" d-none d-md-block">
-        <Row
+      <Row
+        style={{
+          backgroundColor: "gray",
+          minHeight: "5vh",
+          alignItems: "center",
+        }}>
+        <div
           style={{
-            backgroundColor: "gray",
-            left: "0px",
-            minWidth: "max-content",
-          }}
-        >
-          <Col sm={1} style={{ fontSize: "1.5rem" }}>
-            home
-          </Col>
-
-          <Col
-            sm={2}
-            onClick={(e) => {
-              e.stopPropagation();
-            }}
-            style={{
-              fontSize: "1rem",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "flex-start",
-              position: "relative",
-            }}
-          >
-            <div
-              onClick={(e) => {
-                e.stopPropagation();
-                const SubMenus = document.querySelectorAll(".NavItem");
-                for (let index = 0; index < SubMenus.length; index++) {
-                  SubMenus[index].children[1].style.display = "none";
-                }
-                params.BackDropRef.current.classList.add("BackDropActivated");
-                document.querySelectorAll(".categories")[0].style.display =
-                  "flex";
-                document.querySelectorAll(
-                  ".categories"
-                )[0].children[1].style.display = "none";
-              }}
-              style={{ width: "100%", cursor: "pointer" }}
-            >
-              categories
-            </div>
-          </Col>
-
-          <Col
-            sm={1}
-            onClick={(e) => {
-              e.stopPropagation();
-            }}
-            className="NavItem"
-            style={{
-              fontSize: "1rem",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "flex-start",
-              position: "relative",
-            }}
-          >
-            <div
-              onClick={(e) => {
-                e.stopPropagation();
-                const SubMenus = document.querySelectorAll(".NavItem");
-                for (let index = 0; index < SubMenus.length; index++) {
-                  SubMenus[index].children[1].style.display = "none";
-                }
-                document.querySelectorAll(".categories")[0].style.display =
-                  "none";
-                console.log(e.target.parentElement.parentElement);
-                console.log(
-                  e.target.parentElement.parentElement.getBoundingClientRect()
-                    .left
-                );
-                params.BackDropRef.current.classList.add("BackDropActivated");
-                e.target.parentElement.children[1].style.display = "block";
-              }}
-              style={{ width: "100%" }}
-            >
-              profile
-            </div>
-
-            <div
-              style={{
-                minHeight: "400px",
-                minWidth: "400px",
-                border: "2px solid orange",
-                position: "absolute",
-                top: "5vh",
-                left: "0px",
-                backgroundColor: "orange",
-                zIndex: "300",
-              }}
-            >
-              profile items
-            </div>
-          </Col>
-          <Col
-            sm={1}
-            onClick={(e) => {
-              e.stopPropagation();
-            }}
-            className="NavItem"
-            style={{
-              fontSize: "1rem",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "flex-start",
-              position: "relative",
-            }}
-          >
-            <div
-              onClick={(e) => {
-                e.stopPropagation();
-                const SubMenus = document.querySelectorAll(".NavItem");
-                for (let index = 0; index < SubMenus.length; index++) {
-                  SubMenus[index].children[1].style.display = "none";
-                }
-                document.querySelectorAll(".categories")[0].style.display =
-                  "none";
-                console.log(e.target.parentElement.children[1]);
-                params.BackDropRef.current.classList.add("BackDropActivated");
-                e.target.parentElement.children[1].style.display = "block";
-              }}
-              style={{ width: "100%" }}
-            >
-              Products
-            </div>
-
-            <div
-              style={{
-                minHeight: "400px",
-                minWidth: "400px",
-                border: "2px solid orange",
-                position: "absolute",
-                top: "5vh",
-                left: "0px",
-                backgroundColor: "orange",
-                zIndex: "300",
-              }}
-            >
-              Products items
-            </div>
-          </Col>
-          <Col sm={{ offset: 5, span: 1 }}>
-            <Link to="/LogIn">LogIn</Link>
-          </Col>
-        </Row>
-        <Row className="categories" style={{ backgroundColor: "orange" }}>
-          <Col
-            sm={2}
-            onClick={(e) => {
-              e.stopPropagation();
-              console.log(
-                document.querySelectorAll(".categories")[0].children[1]
-              );
-              document.querySelectorAll(
-                ".categories"
-              )[0].children[1].style.display = "block";
-            }}
-          >
-            sadasda
-          </Col>
-          <Col sm={10} style={{ diaplay: "none" }}>
-            sdads
-          </Col>
-        </Row>
-      </div>
-      {/*  small screen navbar start */}
-      <Row className=" d-md-none  d-flex" style={{ backgroundColor: "gray" }}>
-        <Col
-          className=" d-block"
-          style={{ fontSize: "1.5rem", maxWidth: "fit-content" }}
-        >
+            fontSize: "1.5rem",
+            textAlign: "start",
+            width: "fit-content",
+          }}>
           home
-        </Col>
-
-        <Col
-          className=" d-block"
-          style={{ fontSize: "1.5rem", maxWidth: "fit-content" }}
-        >
-          home
-        </Col>
-
-        <Col
-          xs={12}
-          className=" d-block ms-0 ms-sm-auto"
-          style={{ fontSize: "1.5rem", maxWidth: "fit-content" }}
-        >
-          <Link to="/LogIn">LogIn</Link>
-        </Col>
+        </div>
+        <div
+          style={{
+            fontSize: "1rem",
+            textAlign: "start",
+            width: "fit-content",
+            position: "relative",
+          }}>
+          categories
+          <div
+            style={{
+              position: "absolute",
+              left: "-90%",
+              minWidth: "400px",
+              minHeight: "400px",
+              backgroundColor: "red",
+            }}></div>
+        </div>
       </Row>
-       {/*  small screen navbar end */}
     </Container>
   );
 };
---
+++ frontend/src/screens/Home1.js
@@ -0,0 +1,274 @@
+import React, { useRef } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const Home = (params) => {
+  return (
+    <Container>
+      <div className=" d-none d-md-block">
+        <Row
+          style={{
+            backgroundColor: "gray",
+            left: "0px",
+            minWidth: "max-content",
+          }}
+        >
+          <Col sm={1} style={{ fontSize: "1.5rem" }}>
+            home
+          </Col>
+
+          <Col
+            sm={2}
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+            style={{
+              fontSize: "1rem",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "flex-start",
+              position: "relative",
+            }}
+          >
+            <div
+              onClick={(e) => {
+                e.stopPropagation();
+                if ( params.BackDropRef.current.classList.contains("BackDropActivated")) {
+                 
+
+                  const SubMenus = document.querySelectorAll(".NavItem");
+          for (let index = 0; index < SubMenus.length; index++) {
+            SubMenus[index].children[1].style.display = "none";
+          }
+          document.querySelectorAll(".categories")[0].style.display = "none";
+          console.log("backdrop clicked");
+
+          console.log(document.querySelectorAll(".NavItem"));
+
+          params.BackDropRef.current.classList.remove("BackDropActivated");
+                  
+                } else {
+                  const SubMenus = document.querySelectorAll(".NavItem");
+                  for (let index = 0; index < SubMenus.length; index++) {
+                    SubMenus[index].children[1].style.display = "none";
+                  }
+                  
+                  params.BackDropRef.current.classList.add("BackDropActivated");
+                  document.querySelectorAll(".categories")[0].style.display =
+                    "flex";
+                  document.querySelectorAll(
+                    ".categories"
+                  )[0].children[1].style.display = "none";
+                  
+                }
+               
+              }}
+              style={{ width: "100%", cursor: "pointer" }}
+            >
+              categories
+            </div>
+          </Col>
+
+          <Col
+            sm={1}
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+            className="NavItem"
+            style={{
+              fontSize: "1rem",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "flex-start",
+              position: "relative",
+            }}
+          >
+            <div
+              onClick={(e) => {
+                e.stopPropagation();
+
+                if ( params.BackDropRef.current.classList.contains("BackDropActivated")) {
+                 
+
+                  const SubMenus = document.querySelectorAll(".NavItem");
+          for (let index = 0; index < SubMenus.length; index++) {
+            SubMenus[index].children[1].style.display = "none";
+          }
+          document.querySelectorAll(".categories")[0].style.display = "none";
+          console.log("backdrop clicked");
+
+          console.log(document.querySelectorAll(".NavItem"));
+
+          params.BackDropRef.current.classList.remove("BackDropActivated");
+                  
+                } else {
+                  const SubMenus = document.querySelectorAll(".NavItem");
+                for (let index = 0; index < SubMenus.length; index++) {
+                  SubMenus[index].children[1].style.display = "none";
+                }
+                document.querySelectorAll(".categories")[0].style.display =
+                  "none";
+                console.log(e.target.parentElement.parentElement);
+                console.log(
+                  e.target.parentElement.parentElement.getBoundingClientRect()
+                    .left
+                );
+                params.BackDropRef.current.classList.add("BackDropActivated");
+                e.target.parentElement.children[1].style.display = "block";
+                  
+                }
+               
+
+
+                
+              }}
+              style={{ width: "100%" }}
+            >
+              profile
+            </div>
+
+            <div
+              style={{
+                minHeight: "400px",
+                minWidth: "400px",
+                border: "2px solid orange",
+                position: "absolute",
+                top: "5vh",
+                left: "0px",
+                backgroundColor: "orange",
+                zIndex: "300",
+              }}
+            >
+              profile items
+            </div>
+          </Col>
+          <Col
+            sm={1}
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+            className="NavItem"
+            style={{
+              fontSize: "1rem",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "flex-start",
+              position: "relative",
+            }}
+          >
+            <div
+              onClick={(e) => {
+                e.stopPropagation();
+                if ( params.BackDropRef.current.classList.contains("BackDropActivated")) {
+                 
+
+                  const SubMenus = document.querySelectorAll(".NavItem");
+          for (let index = 0; index < SubMenus.length; index++) {
+            SubMenus[index].children[1].style.display = "none";
+          }
+          document.querySelectorAll(".categories")[0].style.display = "none";
+          console.log("backdrop clicked");
+
+          console.log(document.querySelectorAll(".NavItem"));
+
+          params.BackDropRef.current.classList.remove("BackDropActivated");
+                  
+                } else {
+                  const SubMenus = document.querySelectorAll(".NavItem");
+                for (let index = 0; index < SubMenus.length; index++) {
+                  SubMenus[index].children[1].style.display = "none";
+                }
+                document.querySelectorAll(".categories")[0].style.display =
+                  "none";
+                console.log(e.target.parentElement.parentElement);
+                console.log(
+                  e.target.parentElement.parentElement.getBoundingClientRect()
+                    .left
+                );
+                params.BackDropRef.current.classList.add("BackDropActivated");
+                e.target.parentElement.children[1].style.display = "block";
+                  
+                }
+              }}
+              style={{ width: "100%" }}
+            >
+              Products
+            </div>
+
+            <div
+              style={{
+                minHeight: "400px",
+                minWidth: "400px",
+                border: "2px solid orange",
+                position: "absolute",
+                top: "5vh",
+                left: "0px",
+                backgroundColor: "orange",
+                zIndex: "300",
+              }}
+            >
+              Products items
+            </div>
+          </Col>
+          <Col sm={{ offset: 5, span: 1 }}>
+            <Link to="/LogIn">LogIn</Link>
+          </Col>
+        </Row>
+        <Row className="categories" style={{ backgroundColor: "orange" }}>
+          <Col
+            sm={2}
+            onClick={(e) => {
+              e.stopPropagation();
+              console.log(
+                document.querySelectorAll(".categories")[0].children[1]
+              );
+              document.querySelectorAll(
+                ".categories"
+              )[0].children[1].style.display = "block";
+            }}
+          >
+            sadasda
+          </Col>
+          <Col sm={10} style={{ diaplay: "none" }}>
+            sdads
+          </Col>
+        </Row>
+      </div>
+      {/*  small screen navbar start */}
+      <Row className=" d-md-none  d-flex" style={{ backgroundColor: "gray" }}>
+        <Col
+          className=" d-block"
+          style={{ fontSize: "1.5rem", maxWidth: "fit-content" }}
+        >
+          home
+        </Col>
+
+        <Col
+          className=" d-flex SmallMenu"
+          style={{ fontSize: "1rem", maxWidth: "fit-content", alignContent:'center',alignItems:'center' }}
+          onClick={(e)=>{
+            if (params.BackDropRef.current.classList.contains("BackDropActivated")) {
+              
+
+            } else {
+              
+            }
+          }}
+        >
+          Catigories
+        </Col>
+
+        <Col
+          xs={12}
+          className=" d-block ms-0 ms-sm-auto"
+          style={{ fontSize: "1rem", maxWidth: "fit-content" }}
+        >
+          <Link to="/LogIn">LogIn</Link>
+        </Col>
+      </Row>
+       {/*  small screen navbar end */}
+    </Container>
+  );
+};
+
+export default Home;
