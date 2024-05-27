7ca91391f4632483137d6062a98f73d0d8a2ce07
Ali Mohammed <104313196+engali1986@users.noreply.github.com>
2024-05-27 19:37:37 +0300

    SmallScreenNavbar

--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -3,12 +3,9 @@ import { Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const Home = (params) => {
-
-  
   return (
     <Container>
-      <Row className="NavBarBig"
-        >
+      <Row className="NavBarBig">
         <div
           style={{
             fontSize: "1.5rem",
@@ -17,26 +14,54 @@ const Home = (params) => {
           }}>
           home
         </div>
-        <div 
+        <div
           style={{
             fontSize: "1rem",
             textAlign: "start",
             width: "fit-content",
             position: "relative",
-          }} onClick={(e)=>{
-            e.stopPropagation()
-            console.log(e.target)
+          }}
+          onClick={(e) => {
+            e.stopPropagation();
+            document.getElementsByClassName("Services")[0].lastChild.style.display = "none";
+            if (document.getElementsByClassName("Categories")[0].style.height === "90vh") {
+              document.getElementsByClassName("Categories")[0].style.height = "0vh";
+              document.getElementsByClassName("Categories")[0].style.overflowY = "hidden";
+            } else {
+              document.getElementsByClassName("Categories")[0].style.height = "90vh";
+              document.getElementsByClassName("Categories")[0].style.overflowY = "scroll";
+            }
+            console.log(e.target);
           }}>
           categories
-         
+        </div>
+        <div
+          className="Services"
+          style={{
+            fontSize: "1rem",
+            textAlign: "start",
+            width: "fit-content",
+            position: "relative",
+          }}
+          onClick={(e) => {
+            e.stopPropagation();
+            document.getElementsByClassName("Categories")[0].style.height = "0vh";
+            document.getElementsByClassName("Categories")[0].style.overflowY = "hidden";
+            console.log(e.target.lastChild);
+            if (e.target.lastChild.style.display === "block") {
+              e.target.lastChild.style.display = "none";
+            } else {
+              e.target.lastChild.style.display = "block";
+            }
+
+            console.log(e.target);
+          }}>
+          Services
+          <div style={{ position: "absolute", minHeight: "400px", minWidth: "400px", top: "100%", left: "0%", display: "none" }}></div>
         </div>
       </Row>
-      <Row >
-        <div className="Catigories" style={{width:'fit-content', height:'0vh', overflowY:'hidden'}}>
-        adsasd
-
-        </div>
-       
+      <Row>
+        <div className="Categories">adsasd</div>
       </Row>
     </Container>
   );
