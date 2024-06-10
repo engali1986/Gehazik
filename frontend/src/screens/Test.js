bbecbf32d0bc38fbbeacabf3e64492c9a2b1b1b0
Ali Mohammed <104313196+engali1986@users.noreply.github.com>
2024-06-10 18:33:45 +0300

    SmallScreenNavBar

---
+++ frontend/src/screens/Test.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <div style={{display:'flex',width:'100%', height:'400px', border:'2px solid black', overflow:'hidden'}}>
+        <div style={{display:'flex',minWidth:'120%', height:'400px', border:'2px solid red'}}>
+
+       
+        
+        <div style={{display:'inline-block', minWidth:"40%", height:'400px', backgroundColor:'green'}}>
+
+        </div>
+        <div style={{display:'inline-block', minWidth:"40%", height:'400px', backgroundColor:'red'}}>
+
+        </div>
+        <div style={{display:'inline-block', minWidth:"40%", height:'400px', backgroundColor:'blue'}}>
+
+        </div>
+
+        </div>
+
+
+    </div>
+  )
+}
+
+export default Test
