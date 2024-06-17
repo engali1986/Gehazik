d394fd8fc7579eed470eb737c21b6a655e2318fa
Ali Mohammed <104313196+engali1986@users.noreply.github.com>
2024-06-17 19:35:17 +0300

    SmallScreenNavBar

--- frontend/src/screens/ContactUs.js
+++ frontend/src/screens/ContactUs.js
@@ -9,9 +9,40 @@ const ContactUs = () => {
             <div style={{fontSize:'2rem', wordWrap:'break-word'}}>
                 ContactUs
             </div>
-            <a href='mailto:engaligulf@gmail.com'>Email us</a>
+            
 
             </Col>
+        </Row>
+        <Row style={{minHeight:'20vh'}}>
+          <Col className=' p-1' xs={12} md={6} style={{display:'flex', flexDirection:'column' ,alignContent:'space-around', justifyContent:'space-around', borderRadius:'5%'}}>
+            <div  style={{border:'1px solid gray', borderRadius:'5px'}}  >
+              <h1>
+              <i class="fa-solid fa-envelope"></i>
+              </h1>
+              <h2>
+                Send E-mail
+              </h2>
+            <a href='mailto:engaligulf@gmail.com'>Email us</a>
+
+            </div>
+          
+          </Col>
+
+          <Col className=' p-1' xs={12} md={6} style={{display:'flex', flexDirection:'column' ,alignContent:'space-around', justifyContent:'space-around', borderRadius:'5%'}}>
+            <div  style={{border:'1px solid gray', borderRadius:'5px'}}  >
+              <h1>
+              <i class="fa-solid fa-phone"></i>
+              </h1>
+              <h2>
+                Call us
+              </h2>
+            <a href='tel:+201101806523'>+201101806523</a>
+
+            </div>
+          
+          </Col>
+          
+
         </Row>
         
         
