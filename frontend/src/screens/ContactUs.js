f71a9bec60fd5557b8666ffc6571e4909c8fd000
Ali Mohammed <104313196+engali1986@users.noreply.github.com>
2024-06-29 20:44:23 +0300

    SmallScreenNavBar

--- frontend/src/screens/ContactUs.js
+++ frontend/src/screens/ContactUs.js
@@ -1,54 +1,38 @@
-import React from 'react'
-import {Container,Row,Col} from"react-bootstrap"
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 const ContactUs = () => {
   return (
-    <Container>
-        <Row style={{marginTop:'20%'}}>
-            <Col xs={12} >
-            <div style={{fontSize:'2rem', wordWrap:'break-word'}}>
-                ContactUs
-            </div>
-            
-
-            </Col>
-        </Row>
-        <Row style={{minHeight:'20vh'}}>
-          <Col className=' p-1' xs={12} md={6} style={{display:'flex', flexDirection:'column' ,alignContent:'space-around', justifyContent:'space-around', borderRadius:'5%'}}>
-            <div  style={{border:'1px solid gray', borderRadius:'5px'}}  >
-              <h1>
+    <Container style={{ marginTop: "25%" }}>
+      <Row >
+        <Col xs={12}>
+          <div style={{ fontSize: "3rem", wordWrap: "break-word", fontWeight: "bold" }}>ContactUs</div>
+          <div style={{ fontSize: "1.5rem", wordWrap: "break-word" }}>For any quires please reach us any time 24/7</div>
+        </Col>
+      </Row>
+      <Row style={{ minHeight: "20vh" }}>
+        <Col className=" p-1" xs={12} md={6} style={{ display: "flex", flexDirection: "column", alignContent: "space-around", justifyContent: "space-around", borderRadius: "5%" }}>
+          <div style={{ border: "1px solid gray", borderRadius: "5px", boxShadow: "6px 4px 9px 0px rgba(0,0,0,0.5)" }}>
+            <h1>
               <i class="fa-solid fa-envelope"></i>
-              </h1>
-              <h2>
-                Send E-mail
-              </h2>
-            <a href='mailto:engaligulf@gmail.com'>Email us</a>
+            </h1>
+            <h2>Send E-mail</h2>
+            <a href="mailto:engaligulf@gmail.com">Email us</a>
+          </div>
+        </Col>
 
-            </div>
-          
-          </Col>
-
-          <Col className=' p-1' xs={12} md={6} style={{display:'flex', flexDirection:'column' ,alignContent:'space-around', justifyContent:'space-around', borderRadius:'5%'}}>
-            <div  style={{border:'1px solid gray', borderRadius:'5px'}}  >
-              <h1>
+        <Col className=" p-1" xs={12} md={6} style={{ display: "flex", flexDirection: "column", alignContent: "space-around", justifyContent: "space-around", borderRadius: "5%" }}>
+          <div style={{ border: "1px solid gray", borderRadius: "5px", boxShadow: "6px 4px 9px 0px rgba(0,0,0,0.5)" }}>
+            <h1>
               <i class="fa-solid fa-phone"></i>
-              </h1>
-              <h2>
-                Call us
-              </h2>
-            <a href='tel:+201101806523'>+201101806523</a>
-
-            </div>
-          
-          </Col>
-          
-
-        </Row>
-        
-        
-       
+            </h1>
+            <h2>Call us</h2>
+            <a href="tel:+201101806523">+201101806523</a>
+          </div>
+        </Col>
+      </Row>
     </Container>
-  )
-}
+  );
+};
 
-export default ContactUs
+export default ContactUs;
