751a69b5ac682684553acab62c0a95b0f6795d1c
Ali Mohammed <104313196+engali1986@users.noreply.github.com>
2024-07-15 09:43:41 +0300

    AdminPageModefied

--- frontend/src/screens/Test.js
+++ frontend/src/screens/Test.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React,{useRef} from 'react'
 
 const Test = () => {
+  const InputData=useRef()
+  const Data=useRef()
   return (
     <>
+    <input ref={InputData} type='text'/>
+    <button onClick={(e)=>{
+      e.stopPropagation()
+      console.log(Data.current.value.split(/\t|\n/))
+
+      
+      
+      // console.log(InputData.current.value.split(/\s/))
+      
+      
+      
+     
+
+    }}>
+      Convert to array
+    </button>
+    <textarea ref={Data} cols={20} rows={1}>
+
+    </textarea>
+
    
     </>
   )
