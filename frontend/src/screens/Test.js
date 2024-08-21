52e7316e87c4f75d7039e20e6ae5b957586dda68
Ali Mohammed <104313196+engali1986@users.noreply.github.com>
2024-08-21 14:12:48 +0300

    Test

--- frontend/src/screens/Test.js
+++ frontend/src/screens/Test.js
@@ -3,22 +3,19 @@ import React, { useState, useEffect } from "react";
 const Test = () => {
   return (
     <>
+    <div className="bb">
+      jhjkhljkj
+    </div>
       <button
         onClick={(e) => {
           e.stopPropagation();
-          let arr1 = [
-            { ID: 1, b: 5 },
-            { ID: 2, bb: 4 },
-          ];
-          let arr2 = arr1.filter((item) => {
-            if (item.ID === 1) {
-              return item;
-            }
-          });
-
-          console.log(arr2);
+          window.print("avf")
         }}
-      ></button>
+      > print</button>
+      <div className="AA">
+        njhljlkjljk
+
+      </div>
     </>
   );
 };
