a2782ad7d92202a5e178e371b028a481d76150ee
Ali Mohammed <104313196+engali1986@users.noreply.github.com>
2024-08-21 15:05:43 +0300

    Test

--- frontend/src/screens/Test.js
+++ frontend/src/screens/Test.js
@@ -1,17 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect,useRef } from "react";
+import jsPDF from"jspdf"
 
 const Test = () => {
+  const divRef = useRef();
   return (
     <>
-    <div className="bb">
-      jhjkhljkj
+    <div>
+      <div ref={divRef} style={{ padding: 20 }}>
+        <h1>Hello, World!</h1>
+        <p>This is the content of the div that will be saved as a PDF.</p>
+      </div>
+      <button onClick={()=>{
+        const doc = new jsPDF();
+
+        doc.html(divRef.current, {
+          callback: function (doc) {
+            doc.save('document.pdf');
+          },
+          x: 10,
+          y: 10,
+          width: 180, // Width of content on the page
+          windowWidth: divRef.current.offsetWidth, // HTML width used for rendering
+        });
+      }}>Download as PDF</button>
     </div>
-      <button
-        onClick={(e) => {
-          e.stopPropagation();
-          window.print("avf")
-        }}
-      > print</button>
+      
       <div className="AA">
         njhljlkjljk
 
