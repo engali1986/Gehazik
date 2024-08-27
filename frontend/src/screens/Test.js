286f03b4e8e9f362554e4e48af113a559b057a82
Ali Mohammed <104313196+engali1986@users.noreply.github.com>
2024-08-27 14:23:58 +0300

    Test

--- frontend/src/screens/Test.js
+++ frontend/src/screens/Test.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect,useRef } from "react";
 import jsPDF from"jspdf"
-import {Dropdown} from "react-bootstrap"
+import {Dropdown, Toast, ToastHeader, ToastBody, ToastContainer} from "react-bootstrap"
 const Test = () => {
   const divRef = useRef();
   
@@ -10,27 +10,50 @@ const Test = () => {
       <div className=" flex-grow-1">
         hgjhgjh
       </div>
-    <Dropdown>
-    <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-    Dropdown button
-  </button>
-  <ul className="dropdown-menu">
-    <li><a className="dropdown-item" href="#">Action</a></li>
-    <li><a className="dropdown-item" href="#"><Dropdown>
-    <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-    Dropdown button
-  </button>
-  <ul className="dropdown-menu">
-  <li><a className="dropdown-item" href="#">Action</a></li>
+      <Dropdown>
+      <Dropdown.Toggle variant="success" id="dropdown-basic">
+        Dropdown Button
+      </Dropdown.Toggle>
 
-  </ul>
+      <Dropdown.Menu>
+      <Dropdown>
+      <Dropdown.Toggle variant="success" id="dropdown-basic" style={{width:'100%'}}>
+        a
+      </Dropdown.Toggle>
 
-      </Dropdown></a></li>
-    <li><a className="dropdown-item" href="#">Something else here</a></li>
-  </ul>
+      <Dropdown.Menu>
+        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+      </Dropdown.Menu>
     </Dropdown>
 
     </div>
+
+    <div onClick={(e)=>{
+      e.stopPropagation()
+    }}>
+      jhggkhkjhkj
+    </div>
+    <button type="button" className="btn btn-primary" id="liveToastBtn">Show live toast</button>
+
+<div className="position-fixed bottom-0 end-0 p-3" style={{zIndex:'11'}}>
+  <div id="liveToast" className="toast hide" role="alert" aria-live="assertive" aria-atomic="true">
+    <div className="toast-header">
+      <img src="..." className="rounded me-2" alt="..."/>
+      <strong className="me-auto">Bootstrap</strong>
+      <small>11 mins ago</small>
+      <button type="button" className="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+    </div>
+    <div className="toast-body">
+      Hello, world! This is a toast message.
+    </div>
+  </div>
+</div>
     
  
     
