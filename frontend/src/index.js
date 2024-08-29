73c5ff3fd7e79149a144606b749546dd997270bf
Ali Mohammed <104313196+engali1986@users.noreply.github.com>
2024-08-29 12:24:05 +0300

    Test

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js"
 
 import App from "./App";
 import {
--- frontend/src/screens/Test.js
+++ frontend/src/screens/Test.js
@@ -19,30 +19,16 @@ const Test = () => {
       }}>
         hgjhgjh
       </div>
-      <Dropdown>
-      <Dropdown.Toggle variant="success" id="dropdown-basic" onClick={(e)=>{
-        e.stopPropagation()
-        toast.error("Success Notification !");
-      }}>
-        Dropdown Button
-      </Dropdown.Toggle>
-
-      <Dropdown.Menu>
-      <Dropdown>
-      <Dropdown.Toggle variant="success" id="dropdown-basic" style={{width:'100%'}}>
-        a
-      </Dropdown.Toggle>
-
-      <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
+      <div className="dropdown">
+  <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    Dropdown button
+  </button>
+  <ul className="dropdown-menu">
+    <li><a className="dropdown-item" href="#">Action</a></li>
+    <li><a className="dropdown-item" href="#">Another action</a></li>
+    <li><a className="dropdown-item" href="#">Something else here</a></li>
+  </ul>
+</div>
 
     </div>
 
@@ -51,22 +37,29 @@ const Test = () => {
     }}>
       jhggkhkjhkj
     </div>
-    <button type="button" className="btn btn-primary" id="liveToastBtn">Show live toast</button>
+    
+    <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Launch demo modal
+</button>
 
-<div className="position-fixed bottom-0 end-0 p-3" style={{zIndex:'11'}}>
-  <div id="liveToast" className="toast hide" role="alert" aria-live="assertive" aria-atomic="true">
-    <div className="toast-header">
-      <img src="..." className="rounded me-2" alt="..."/>
-      <strong className="me-auto">Bootstrap</strong>
-      <small>11 mins ago</small>
-      <button type="button" className="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-    </div>
-    <div className="toast-body">
-      Hello, world! This is a toast message.
+
+<div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div className="modal-dialog">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div className="modal-body">
+        ...
+      </div>
+      <div className="modal-footer">
+        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" className="btn btn-primary">Save changes</button>
+      </div>
     </div>
   </div>
 </div>
-    
  
     
     </>
