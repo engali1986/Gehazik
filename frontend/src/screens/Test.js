878eeab2d22643cf36bc5db805c2770710f97c0d
Ali Mohammed <104313196+engali1986@users.noreply.github.com>
2024-08-31 15:52:48 +0300

    Test

--- frontend/src/screens/Test.js
+++ frontend/src/screens/Test.js
@@ -1,47 +1,24 @@
-import React, { useState, useEffect,useRef } from "react";
-import jsPDF from"jspdf"
-import {Dropdown,Row} from "react-bootstrap"
-import { toast} from 'react-toastify';
-  import "react-toastify/dist/ReactToastify.css";
+import React, { useState} from "react";
+
+const AddAdress=({ Address, SetAddress })=>{
+  return(
+    <>
+    <input onChange={(e)=>{
+      SetAddress({...Address,FirstName:e.target.value})
+    }} type="text"  value={Address.FirstName} placeholder="Add First name" />
+    </>
+  )
+ }
+
 const Test = () => {
-  const divRef = useRef();
+ const [Address,SetAddress]=useState({FirstName:''})
+
+ 
   
   return (
-    <>
-    <div id="Printed">
-      jhkljkjlkjlkk
-      <div style={{color:'red'}}>
-        jkoo099999
-      </div>
-
-    </div>
-    <button onClick={(e)=>{
-      e.stopPropagation()
-      let mywindow = window.open('', 'PRINT', 'height=650,width=900,top=100,left=150');
-
-  // mywindow.document.write(`<html><head><title>aaa</title>`);
-  // mywindow.document.write('</head><body >');
-  mywindow.document.write(document.getElementById("Printed").innerHTML);
-  // mywindow.document.write('</body></html>');
-  // mywindow.document.close();
-  console.log(mywindow)
-  console.log(mywindow.document)
-  // mywindow.print();
-  // mywindow.close();
-  console.log(window.btoa(mywindow.document))
-  console.log(window.atob(window.btoa(mywindow.document)))
-  let File= new Blob([mywindow.document],{type:'application/pdf'})
-  console.log(File)
-  const downloadLink = document.createElement("a")
-downloadLink.href = 'data:application/octet-stream;base64,' + window.btoa(mywindow.document)
-downloadLink.download = "convertedPDFFile.pdf"
-downloadLink.click()
-    }}>
-      Print
-    </button>
-  
-    
-    </>
+ <>
+ <AddAdress Address={Address} SetAddress={SetAddress}/>
+ </>
   );
 };
 
