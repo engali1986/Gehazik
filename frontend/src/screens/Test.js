1becc4349c0405b70e58f282d346f16139d736e1
Ali Mohammed <104313196+engali1986@users.noreply.github.com>
2024-10-11 15:29:26 +0300

    Update Test.js

--- frontend/src/screens/Test.js
+++ frontend/src/screens/Test.js
@@ -1,44 +1,80 @@
 import React, { useState,useEffect, useRef,useContext} from "react";
 import io from 'socket.io-client';
 import {LanguageContext} from "../Context/LanguageContext";
-import {Container,Row, Col} from "react-bootstrap"
+import {Row, Col, Button, Form} from "react-bootstrap"
 const socket = io('https://gehazik-server.onrender.com'); // Connect to the Node.js server
-const DisplayData=({Data, SetData})=>{
-  return(
-    <>
-   <button onClick={(e)=>{
-    e.stopPropagation()
-    let x=5
-    for (let index = 0; index < 5; index++) {
-      console.log(index)
-      if(index===1){
-        return x
-      }else{
 
-      }
-      
-    }
-   }}>
-    click
-
-   </button>
-    </>
-  )
-}
 const Test = ({globalState}) => {
-  const {Language,ToggleLanguage}=useContext(LanguageContext)
-  const [Data,SetData]=useState(["aa","bb","CC"])
+  const [Options, SetOptions] = useState([]);
 
-  
- 
- 
+  const addOption = () => {
+    SetOptions([...Options, { Color: '', Size: '', Qty:0 }]);
+  };
+
+  const removeOption = (index) => {
+    const NewOptions = [...Options];
+    NewOptions.splice(index, 1);
+    SetOptions(NewOptions);
+  };
+
+  const handleOptionChange = (index, field, value) => {
+    const NewOptions = [...Options];
+    NewOptions[index][field] = value;
+    SetOptions(NewOptions);
+  };
+
   return (
-   
-     <>
-     <DisplayData Data={Data} SetData={SetData}/>
-     </>
-    
- 
+    <div className="OptionsContainer">
+      <Row>
+        <Col xs={8} md={10} onClick={(e)=>{
+          e.stopPropagation()
+          console.log(Options)
+        }}>
+          <div>Product Options</div>
+        </Col>
+        <Col xs={4} md={2}>
+          <div onClick={addOption} style={{ cursor: 'pointer', color: 'blue' }}>
+            Add Option
+          </div>
+        </Col>
+      </Row>
+
+      {Options.map((option, index) => (
+        <Row key={index} className="mt-3 align-items-center">
+          
+          <Col>
+            <input
+              type="text"
+              placeholder="Color"
+              value={option.Color}
+              onChange={(e) => handleOptionChange(index, 'Color', e.target.value)}
+            />
+          </Col>
+          <Col>
+            <input
+              type="text"
+              placeholder="Size"
+              value={option.Size}
+              onChange={(e) => handleOptionChange(index, 'Size', e.target.value)}
+            />
+          </Col>
+          <Col>
+            <input
+              type="number"
+              placeholder="Qty"
+              value={option.Qty}
+              onChange={(e) => handleOptionChange(index, 'Qty', e.target.value)}
+            />
+          </Col>
+          <Col>
+            <Button variant="danger" onClick={() => removeOption(index)}>
+              Remove
+            </Button>
+          </Col>
+        </Row>
+      ))}
+      
+    </div>
   );
 };
 export default Test;
