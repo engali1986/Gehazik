6cb2405b82bf2d3d520c457639be0c3d00c750fe
Ali Mohammed <104313196+engali1986@users.noreply.github.com>
2024-12-21 09:44:26 +0200

    Update AddOrder.js

--- backend/DBConnection/Users/AddOrder.js
+++ backend/DBConnection/Users/AddOrder.js
@@ -1,89 +1,112 @@
-// AddOrder is used to Add orders to Database
 import { MongoClient, ObjectId } from "mongodb";
-const Object=new ObjectId()
-const uri =
-    "mongodb+srv://engaligulf:Cossacks%401@cluster0.fj9bpe7.mongodb.net/?maxIdleTimeMS=5000";
+
+const uri = "mongodb+srv://engaligulf:Cossacks%401@cluster0.fj9bpe7.mongodb.net/?maxIdleTimeMS=5000";
 const client = new MongoClient(uri);
-// AddOrder function is used to add Order to database
+
 const AddOrder = async (OrderData) => {
     try {
-        console.log("AddOrder file 0");
-    await client.connect();
-    // console.log("Connection established ");
-    console.log(OrderData)
-    //  Next we check available Qty for each product
-    let results=0
-    for (let index = 0; index < OrderData.OrderDetails.length; index++) {
-        console.log("AddOrder file 1 Order.OrderDetails", [index]);
-        console.log(OrderData.OrderDetails[index])
-        const res=await client.db("Gehazik").collection("Products").findOne({_id:new ObjectId(OrderData.OrderDetails[index].ID), "ProductOptions.Color":OrderData.OrderDetails[index].Color, "ProductOptions.Size":OrderData.OrderDetails[index].Size},{projection:{ProductOptions:1}}).then(res=>{
-            console.log("AddOrder file 2 Order.OrderDetails", [index]);
-            console.log(res)
-            if (typeof res==="object" && res._id) {
-                results=results+1
-            } else {
-                
+        // Establish database connection
+        await client.connect();
+        const db = client.db("Gehazik");
+        const productsCollection = db.collection("Products");
+        const ordersCollection = db.collection("Orders");
+
+        const session = client.startSession();
+
+        let response;
+
+        // Start transaction
+        await session.withTransaction(async () => {
+            // Step 1: Validate product availability
+            const productChecks = await Promise.all(
+                OrderData.OrderDetails.map(async (item) => {
+                    const product = await productsCollection.findOne(
+                        {
+                            _id: new ObjectId(item.ID),
+                            "ProductOptions.$.Color": item.Color,
+                            "ProductOptions.$.Size": item.Size,
+                            "ProductOptions.$.Qty": { $gte: item.Qty }, // Ensure sufficient stock
+                        },
+                        { projection: { "ProductOptions.$": 1 } }
+                    );
+                    console.log(product)
+
+                    return product ? { valid: true, item } : { valid: false, item };
+                })
+            );
+
+            // Check for any invalid products
+            const invalidProducts = productChecks.filter((check) => !check.valid);
+            if (invalidProducts.length > 0) {
+                console.log("Invalid products found:", invalidProducts);
+                throw new Error("One or more products are unavailable or have insufficient stock.");
             }
-            return res
-        }).catch(err=>{
-            console.log(err)
-            return "Product not found"
-        })
-        
-    }
-    console.log("AddOrder file 3 availability results", results, OrderData.OrderDetails.length)
-    if (OrderData.OrderDetails.length===results) {
-        // Next we will adjust available Qty of each item in DB
-        const updateResults = await Promise.all(OrderData.OrderDetails.map(async (item) => {
-            const updateResult = await client.db("Gehazik").collection("Products").updateOne({
-                _id: new ObjectId(item.ID),
-                "ProductOptions.Color": item.Color,
-                "ProductOptions.Size": item.Size
-            }, {
-                $inc: { "ProductOptions.$.Qty": -item.Qty, "ProductOptions.$.OrderedQty":item.Qty }
-            });
 
-            if (updateResult.modifiedCount !== 1) {
-                console.log(`Failed to update product: ID=${item.ID}, Color=${item.Color}, Size=${item.Size}`);
-                return false;
+            // Step 2: Update product stock in a single bulk operation
+            const bulkOperations = productChecks.map(({ item }) => ({
+                updateOne: {
+                    filter: {
+                        _id: new ObjectId(item.ID),
+                        "ProductOptions.Color": item.Color,
+                        "ProductOptions.Size": item.Size,
+                    },
+                    update: {
+                        $inc: {
+                            "ProductOptions.$.Qty": -item.Qty,
+                            "ProductOptions.$.OrderedQty": item.Qty,
+                        },
+                    },
+                },
+            }));
+
+            const bulkResult = await productsCollection.bulkWrite(bulkOperations, { session });
+            if (!bulkResult.modifiedCount) {
+                throw new Error("Failed to update stock for one or more products.");
             }
-            return true;
-        }));
 
-        // If any update failed, terminate the process
-        if (updateResults.includes(false)) {
-            console.log("Failed to update one or more products.");
-            return "Failed to update stock";
-        }else{
-            console.log("Qty updated")
-        }
+            // Step 3: Insert order into the Orders collection
+            const order = {
+                OrderedBy: OrderData.ClientName,
+                OrderedEmail: OrderData.ClientEmail,
+                OrderedDate: new Date(),
+                OrderedPhone: OrderData.Address.Phone,
+                OrderedPayed: false,
+                OrderedPaymentMethod: OrderData.Address.Payment,
+                OrderedItems: OrderData.OrderDetails,
+                OrderedValue: OrderData.OrderValue,
+                OrderedAddress: OrderData.Address,
+                OrderDelivered: false,
+                OrderPayed: false,
+                OrderPaymentDate: "",
+                MerchantPaymentSent: false,
+                MerchantPaymentDate: "",
+                OrderCompleted: false,
+                OrderCancelled: false,
+                OrderCancelledDate: "",
+                CancelReason: "",
+                OrderStatus: [
+                    {
+                        Status: OrderData.Address.Payment === "Vodafone Cash" ? "Waiting Payment" : "On the way",
+                        Date: new Date(),
+                    },
+                ],
+            };
 
-        // let ModefiedQty=0
-        // for (let index = 0; index < OrderData.OrderDetails.length.length; index++) {
-        //     const UpdateQty=await client.db("Gehazik").collection("Products").updateOne({_id:new ObjectId(OrderData.OrderDetails[index].ID), "ProductOptions.Color":OrderData.OrderDetails[index].Color, "ProductOptions.Size":OrderData.OrderDetails[index].Size},{$inc:{ "ProductOptions.$.Qty":-OrderData.OrderDetails[index].Qty}}).then(res=>{
-        //         console.log(res)
-        //         if (res.modifiedCount===1) {
-        //             ModefiedQty=ModefiedQty+1
-        //         }
-        //         return res
-        //     }).catch(err=>{
-        //         console.log(err)
-        //         return "Product not found"
-        //     })
-        // }
-        // console.log("AddOrder file 4 ModefiedQty results", ModefiedQty, OrderData.OrderDetails.length)
-    } else {
-        return "Product not found"
-    }
-   
+            const insertResult = await ordersCollection.insertOne(order, { session });
+            if (!insertResult.insertedId) {
+                throw new Error("Failed to add order to Orders collection.");
+            }
+
+            response = insertResult
+        });
+
+        // Return success response
+        console.log(response)
+        return response;
     } catch (error) {
-        console.log("AddOrder file 10 error");
-        console.log(error)
-        return "Connection error"
-        
-    }
-    
-    
-    
-}
-export default AddOrder
+        console.error("Error in AddOrder:", error.message);
+        return "Order Not Added"
+    } 
+};
+
+export default AddOrder;
